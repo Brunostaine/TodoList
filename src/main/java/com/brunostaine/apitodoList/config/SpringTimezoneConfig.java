3cb6831998eb12be7bb8102b8c4c180c3f82e0b9
Bruno Staine <brunostaine@hotmail.com>
2023-12-19 14:35:28 -0300

    configurações iniciais

---
+++ src/main/java/com/brunostaine/apitodoList/config/SpringTimezoneConfig.java
@@ -0,0 +1,15 @@
+package com.brunostaine.apitodoList.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimezoneConfig {
+    @PostConstruct
+    public void timezoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Ameriaca/Sao_Paulo"));
+    }
+
+}
